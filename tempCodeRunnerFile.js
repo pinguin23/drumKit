48f2a7cd39b4d3ecd1e4fc32d095a1919709df96
pinguin23 <132230681+pinguin23@users.noreply.github.com>
2023-08-11 22:34:51 +0300

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+for (let index = 0; index < 7; index++) {
+//    document.querySelectorAll("button")[index].addEventListener("click", handleClick);
+   
+//    function handleClick(){
+//       alert("Hi");
+//    }
+// }
